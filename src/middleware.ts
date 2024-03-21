efdc7d31feaadcdb9e92c1b2ca9be03d5da8fa98
MaheshtheDev <mahesh.svmr@gmail.com>
2024-03-20 18:17:44 -0700

    Refactor middleware and update signIn page

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,12 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (token === null) {
-        return false;
-      }
       return true;
     },
   },
   pages: {
-    signIn: "/", 
-  }
+    signIn: "/",
+  },
 });
 
 export const config = {
